9f9cdb916401c5380c085ed8389060e195712a09
dmamira <miramami@outlook.jp>
2019-12-24 11:53:59 +0900

    fixed some bugs

--- contracts/paymentAcceptance.sol
+++ contracts/paymentAcceptance.sol
@@ -1,7 +1,7 @@
-pragma solidity >=0.4.20;
+pragma solidity ^0.5.15;
 pragma experimental ABIEncoderV2;
-import "/home/miraidai/point_system/contracts/point_system.sol";
-import "./Ownable.sol";
+import "./point_system2.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/ownership/Ownable.sol";
 
 contract ERC20CoinInterface{
   function transferFrom(address from, address to, uint tokens) public returns (bool success);
@@ -9,40 +9,43 @@ contract ERC20CoinInterface{
   function transfer(address recipient, uint256 amount) public returns (bool);
 }
 
-contract PaymentAcceptance is point_system{
-    address ThanksTokenAddress = 0x692a70D2e424a56D2C6C27aA97D1a86395877b3A;
-    address SorryTokenAddress = 0x0FdF4894a3b7C5a101686829063BE52Ad45bcfb7;
+contract PaymentAcceptance1 is point_system3{
+    address ThanksTokenAddress = 0x285984aE0b00CBd0D7734928c35678A4C1b6D439;
+    address SorryTokenAddress = 0x1AAaf0e50ed827Ac2d6a3774B7259d818FF18ce1;
+    address basecontract = 0x1AAaf0e50ed827Ac2d6a3774B7259d818FF18ce1;
     ERC20CoinInterface ThanksTokenInterface = ERC20CoinInterface(ThanksTokenAddress);
     ERC20CoinInterface SorryTokenInterface = ERC20CoinInterface(SorryTokenAddress);
     
+    function changebaseContract(address _new) public onlyOwner(){
+        basecontract = _new;
+    } 
+    
     function acceptPaymentForAltCoin(uint _price,address _sender,uint _token) external payable returns(bool){
+        require(msg.sender==basecontract);
     if(_token == 0){
-      uint paymentamount = _price;
       uint allowanceAmount = ThanksTokenInterface.allowance(_sender,address(this));
-      require(allowanceAmount>=paymentamount);
-      bool Check0 = ThanksTokenInterface.transferFrom(_sender,address(this),paymentamount);
+      require(allowanceAmount>=_price*(10**15));
+      bool Check0 = ThanksTokenInterface.transferFrom(_sender,address(this),_price*(10**15));
       return Check0;
     }
    else if(_token == 1){
-       uint PaymentAmount = _price;
        uint allowanceAmount1 = SorryTokenInterface.allowance(_sender,address(this));
-       require(allowanceAmount1>=PaymentAmount);
-       bool Check1 = SorryTokenInterface.transferFrom(_sender,address(this),PaymentAmount);
+       require(allowanceAmount1>=_price*(10**15));
+       bool Check1 = SorryTokenInterface.transferFrom(_sender,address(this),_price*(10**15));
        return Check1;
    }
   }
-  function AcceptPaymentForETH(uint _price, address _sender){
-    uint WeiPrice = _price*10**18;
-    require(msg.value>=WeiPrice);
-    return true;
-  }
-    function ConfirmOfReceipt(uint _finalPrice,uint _Token,address _sellersAddress) internal { 
+    function ConfirmOfReceipt(uint _finalPrice,uint _Token,address payable _sellersAddress) public { 
+      require(msg.sender == basecontract);
       uint transferValue = _finalPrice;
       if(_Token == 0){
-      ThanksTokenInterface.transfer(_sellersAddress,transferValue);
+      ThanksTokenInterface.transfer(_sellersAddress,transferValue*(10**15));
       }
       else if(_Token == 1){
-          SorryTokenInterface.transfer(_sellersAddress,transferValue);
+          SorryTokenInterface.transfer(_sellersAddress,transferValue*(10**15));
+      }
+      else if(_Token == 2){
+          _sellersAddress.transfer(_finalPrice*(10**15));
       }
     }
 }
--- contracts/point_system.sol
+++ contracts/point_system.sol
@@ -1,15 +1,17 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.15;
 
 pragma experimental ABIEncoderV2;
-import "./Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/ownership/Ownable.sol";
+import "./PaymentAcceptance.sol";
 
 contract PaymentAcceptance{
-     function acceptPaymentForAltCoin(uint _finalPrice,address _sender,uint _token) external payable returns(bool);
-     function ConfirmOfReceipt(uint _finalPrice,address _sellersAddress,uint _Token,uint evaluation) public;
+     function ConfirmOfReceipt(uint _finalPrice,uint _Token,address _sellersAddress) public;
+     function acceptPaymentForAltCoin(uint _price,address _sender,uint _token) external payable returns(bool);
+     function AcceptPaymentForETH(uint _price, address _sender) public payable returns(bool);
 }
 
-contract point_system is Ownable {
-    address PaymentAcceptanceAddress = 0x8046085fb6806cAa9b19a4Cd7b3cd96374dD9573;
+contract point_system3 is Ownable {
+    address payable PaymentAcceptanceAddress = 0x15e08fa9FE3e3aa3607AC57A29f92b5D8Cb154A2;
     PaymentAcceptance PaymentAcceptance1 = PaymentAcceptance(PaymentAcceptanceAddress);
 struct product{
   string name;
@@ -29,14 +31,17 @@ mapping(uint=>uint) productToSeller;
 product[] public products;
 seller[] public sellers;
 
-function setPaymentAcceptanceAddress(address _new) public onlyOwner(){
+function setPaymentAcceptanceAddress(address payable _new) public onlyOwner(){
     PaymentAcceptanceAddress = _new;
 }
-function addSeller(string _name) external dupCheck(msg.sender) {
+function addSeller(string calldata _name) external dupCheck(msg.sender) {
   sellers.push(seller(_name,msg.sender,0,0));
 }
-  function addItem(string _productName) external exiCheck(msg.sender){
-    uint productId = products.push(product(_productName,false)) - 1;
+  function addItem(string calldata _productName,uint[] calldata _price,uint[] calldata _token) external exiCheck(msg.sender){
+    uint productId = products.push(product(_productName,false,0x0000000000000000000000000000000000000000,0)) - 1;
+    for(uint i = 0; i<_token.length; i++){
+        products[productId].TokenToPrice[_token[i]] = _price[i];
+    }
     ConnectingSellerWithProduct(productId,msg.sender);
   }
   function ConnectingSellerWithProduct(uint _productId,address _sender) private{
@@ -46,32 +51,35 @@ function addSeller(string _name) external dupCheck(msg.sender) {
       }
     }
   }
-  function Buying(uint _productId,uint _token) public{
+  function getPrice(uint _productId,uint _tokenId) public view returns(uint){
+      uint returnPrice = products[_productId].TokenToPrice[_tokenId];
+      return returnPrice;
+  }
+  function Buying(uint _productId,uint _token) public payable{
     if(_token != 2){
-    products[_productId].PaymentStatus = acceptPaymentForAltCoin(products[_productId].TokenToPrice[_token],msg.sender,_token);
-    products[_productId].finalToken = _token;
-    if(products[_productId].Status == true){
-      products[_productId.Buyer] = msg.sender;
+    products[_productId].PaymentStatus = PaymentAcceptance1.acceptPaymentForAltCoin(products[_productId].TokenToPrice[_token],msg.sender,_token);
+    products[_productId].FinalToken = _token;
+    if(products[_productId].PaymentStatus == true){
+      products[_productId].Buyer = msg.sender;
     }
   }
     else{
-      products[_productId].PaymentStatus = acceptPaymentForETH(products[_productId].TokenToPrice[_token],msg.sender,_token);
-      products[_productId].finalToken = _token;
-      if(products[_productId].Status == true){
-      products[_productId.Buyer] = msg.sender;
-    }
-    }
+    require(msg.value>=products[_productId].TokenToPrice[2]*(10**15));
+    products[_productId].PaymentStatus = true;
+    PaymentAcceptanceAddress.transfer(msg.value);
   }
-  function ConfirmOfReceipit(uint _productId,uint _evaluation) external{
+}
+  function ConfirmOfReceipt(uint _productId,uint _evaluation) external{
       require(products[_productId].Buyer == msg.sender && products[_productId].PaymentStatus == true);
-      uint finalprice = products[_productId].finalToken;
-      address sellerAddress = sellers[productToSeller[_productid]].sellerAddress;
+      uint finalprice = products[_productId].FinalToken;
+      uint finaltoken = products[_productId].FinalToken;
+      address sellerAddress = sellers[productToSeller[_productId]].sellerAddress;
       if(_evaluation == 1){
           sellers[productToSeller[_productId]].good++;
       }else if(_evaluation == 2){
           sellers[productToSeller[_productId]].bad++;
       }
-      PaymentAcceptance.ConfirmOfReceipt(finalprice,finalToken,sellerAddress);
+      PaymentAcceptance1.ConfirmOfReceipt(finalprice,finaltoken,sellerAddress);
   }
   modifier dupCheck(address seller1){
     int check = -5;
